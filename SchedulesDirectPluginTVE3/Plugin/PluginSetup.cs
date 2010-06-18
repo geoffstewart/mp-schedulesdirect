e160cb9bd2a4e4a0f4a4c4db59ae538d1375d067
geoff.b.stewart <geoff.b.stewart@569c822f-6175-60b5-082c-233153567da8>
2010-06-18 00:53:29 +0000

    First checkin for thetvdb.com support in schedules direct plugin

--- SchedulesDirectPluginTVE3/Plugin/PluginSetup.cs
+++ SchedulesDirectPluginTVE3/Plugin/PluginSetup.cs
@@ -13,7 +13,7 @@ namespace SchedulesDirect.Plugin
   public class PluginSetup
   {
     internal const string PLUGIN_NAME = "SchedulesDirect EPG Client";
-    internal const string PLUGIN_VERSION = "1.1";
+    internal const string PLUGIN_VERSION = "1.2";
 
     #region ISetupForm Members
 
